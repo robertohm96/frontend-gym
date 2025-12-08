c959c279be1b51c42a674dea09760dc35e0d2db5
Pow2105 <andreesalvarez2105@gmail.com>
2025-12-07 20:16:13 -0500

    add

--- src/services/api.js
+++ src/services/api.js
@@ -1,25 +1,13 @@
 import axios from 'axios';
 
+
+const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:8080/api';
+
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api', 
+  baseURL: baseURL, 
   headers: {
     'Content-Type': 'application/json',
   },
 });
-
-// --- INTERCEPTOR DE SEGURIDAD ---
-// Inyecta el token en TODAS las peticiones automáticamente
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
 
 export default api;
